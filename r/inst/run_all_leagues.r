20d6087ee39d51d7327eaf6b08543a769b203a66
susabi <zhuojia.dai@gmail.com>
2018-08-09 20:45:50 +1000

    init -> inst

--- r/inst/run_all_leagues.r
+++ r/inst/run_all_leagues.r
@@ -15,19 +15,26 @@ urls = list(
   mls_url = "https://footystats.org/usa/mls/matches",
   superettan_url = "https://footystats.org/sweden/superettan/matches",
   japan1_url = "https://footystats.org/japan/j1-league/matches",
-  colombia1_url = "https://footystats.org/colombia/categoria-primera-a/matches"
+  colombia1_url = "https://footystats.org/colombia/categoria-primera-a/matches",
+  denmark1_url = "https://footystats.org/denmark/superliga/matches"
 )
 
 outpath = "data/"
+locked = c(rep(F,length(leagues)-1),T)
+abc = data.table(league=leagues, url=urls %>% unlist, locked = locked)
+write.csv(abc,"r/init/leagues.csv")
 
-leagues = c("csl","ireland1","sweden1","brazil1","norway1","finland1","mls","sweden2","japan1","colombia1")
+
+
+leagues = c("csl","ireland1","sweden1","brazil1","norway1","finland1","mls","sweden2","japan1","colombia1","denamrk1")
+
 
 res = get_pred_haad(urls$csl_url,file.path(outpath,"csl" %>% paste0(".rds")), nsim=10000)
 res[[3]]
 
 #get_pred_haad(urls$csl_url,file.path("r/shiny","csl" %>% paste0(".rds")))  
 
-mapply(get_pred_haad, urls, file.path(outpath, leagues %>% paste0(".rds")))
+mapply(get_pred_haad, urls, file.path(outpath, leagues %>% paste0(".rds")), locked)
 
 
 get_pred_haad(urls$colombia1_url, file.path(outpath, "colombia1"))
